d662f88e8d3ed00edb88f09579f623d5d940b638
Tirdea Dominic <dominic.tirdea2@gmail.com>
2025-03-20 19:06:31 +0200

    schimbari+adaugare footer in layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,21 +9,31 @@ export const Footer = () => {
     <>
       <div className="flex flex-col items-center md:flex-row md:justify-between text-white bg-stone-800 py-12 px-8 w-full  h-auto">
         <div className="flex flex-col gap-4 items-center md:items-start mb-12 md:mb-0">
-          <h1 className="text-2xl">Sorina Brif</h1>
-          <p>Slogan</p>
-          <p>Adresa</p>
+          <h1 className="text-2xl font-semibold">Sorina Brif</h1>
+          <p className="text-sm">Slogan</p>
+          <p className="text-sm">Adresa</p>
         </div>
         <div className="flex gap-16">
           <div className="flex flex-col gap-2 items-center">
-            <p className="text-xl">Link-urile mele</p>
-            <Link href="/about">Despre mine</Link>
-            <Link href="/terapie">Despre terapie</Link>
-            <Link href={"/contact"}>Contact</Link>
+            <p className="text-xl font-semibold">Link-urile mele</p>
+            <Link href="/about" className="font-sm">
+              Despre mine
+            </Link>
+            <Link href="/terapie" className="font-sm">
+              Despre terapie
+            </Link>
+            <Link href={"/contact"} className="font-sm">
+              Contact
+            </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-xl">Linkuri sociale</p>
-            <Link href={"instagram"}>Instagram</Link>
-            <Link href={"facebook"}>Facebok</Link>
+            <p className="text-xl font-semibold">Linkuri sociale</p>
+            <Link href={"instagram"} className="text-sm">
+              Instagram
+            </Link>
+            <Link href={"facebook"} className="text-sm">
+              Facebok
+            </Link>
           </div>
         </div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,37 +5,53 @@ import { useEffect } from "react";
 import Link from "next/link";
 export const Navbar = () => {
   const [display, setDisplay] = useState(false);
-  const [count, setCount] = useState(0);
+  const [animate, setAnimate] = useState(false);
 
   function onClick() {
     setDisplay(true);
-    setCount(count + 1);
   }
 
+  // useEffect(() => {
+  //   document.addEventListener("click", (e) => {
+  //     let menu = document.getElementById("menu2");
+  //     let buton = document.getElementById("buton");
+  //     if (e.target != buton && e.target != menu) {
+  //       setDisplay(false);
+  //     }
+  //   });
+  // }, []);
+
   useEffect(() => {
-    document.addEventListener("click", (e) => {
-      let menu = document.getElementById("menu2");
-      let buton = document.getElementById("buton");
-      if (e.target != buton && e.target != menu) {
-        setDisplay(false);
-      }
-    });
-  }, []);
+    setTimeout(() => {
+      setAnimate(display);
+    }, 0);
+  }, [display]);
 
   return (
     <>
-      <nav className="hidden md:flex justify-between items-center px-16 py-3">
+      <nav className="hidden bg-rose-100 shadow-sm md:flex justify-between items-center px-16 py-3">
         <div className="flex items-center justify-center text-center">
-          Sorina Brif
+          <Link
+            href={"/"}
+            className="hover:text-red-700 font-bold transition-all"
+          >
+            Sorina Brif
+          </Link>
         </div>
         <div className="flex gap-4">
-          <Link href={"/acasa"} className="p-2">
+          <Link href={"/"} className="p-2 hover:text-red-700 transition-all">
             Acasa
           </Link>
-          <Link href={"/about"} className="p-2">
+          <Link
+            href={"/about"}
+            className="p-2 hover:text-red-700 transition-all"
+          >
             Despre mine
           </Link>
-          <Link href={"/terapie"} className="p-2">
+          <Link
+            href={"/terapie"}
+            className="p-2 hover:text-red-700 transition-all"
+          >
             Despre terapie
           </Link>
           <Link
@@ -47,11 +63,11 @@ export const Navbar = () => {
         </div>
       </nav>
 
-      <nav className="md:hidden relative flex justify-between items-center px-4 py-2 pr-1">
-        <div>Sorina Brif</div>
+      <nav className="md:hidden bg-rose-100 shadow-sm relative flex justify-between items-center px-4 py-2 pr-1">
+        <div className="cursor-pointer font-bold">Sorina Brif</div>
         <button
           onClick={onClick}
-          className="flex items-center justify-center p-1"
+          className="flex items-center cursor-pointer justify-center p-1"
         >
           <svg
             id="buton"
@@ -73,31 +89,66 @@ export const Navbar = () => {
           </svg>
         </button>
       </nav>
-      <div
-        className={`
+      {/* ideea este sa randez prima oara divul mare si apoi cand se randeaza sa 
+      ii dau un translate lui ala mic */}
+      {display && (
+        <div
+          className={`
           transition-all ease-in duration-500
-          ${
-            display ? "opacity-100" : "opacity-0 pointer-events-none"
-          } flex top-0 md:hidden bg-black/50  w-full min-h-screen absolute `}
-      >
-        <div className="absolute right-0 w-7/12 bg-blue-200 flex flex-col items-start min-h-screen gap-4 px-4 pt-4">
-          <Link href={"/acasa"} className="p-2">
-            Acasa
-          </Link>
-          <Link href={"/about"} className="p-2">
-            Despre mine
-          </Link>
-          <Link href={"/terapie"} className="p-2">
-            Despre terapie
-          </Link>
-          <Link
-            href={"/contact"}
-            className="py-2 px-6 w-full text-center bg-red-700 text-white rounded-lg"
-          >
-            Contact
-          </Link>
+          flex top-0 md:hidden bg-black/50  w-full min-h-screen absolute `}
+          onClick={(e) => {
+            let burger = document.getElementById("burger");
+            if (e.target != burger) {
+              setDisplay(false);
+            }
+          }}
+        >
+          {display && (
+            <div
+              id="burger"
+              className={`absolute -right-7/12 transform transition-transform ease-in-out duration-400 ${
+                animate ? "-translate-x-full" : " -translate-x-0 "
+              } w-7/12 bg-rose-100 flex flex-col items-start min-h-screen gap-4 px-4 pt-4`}
+            >
+              <h1 className="p-2 font-bold mb-4">Sorina Brif</h1>
+              <div className="flex flex-col gap-2 mb-8 w-full">
+                <h2 className="border-b border-black border-solid w-full p-2 font-semibold">
+                  Pagini
+                </h2>
+                <Link
+                  href={"/"}
+                  className="px-2 hover:text-red-700 bg-blue transition-all"
+                >
+                  Acasa
+                </Link>
+                <Link
+                  href={"/about"}
+                  className="px-2 hover:text-red-700 transition-all"
+                >
+                  Despre mine
+                </Link>
+                <Link
+                  href={"/terapie"}
+                  className="px-2 hover:text-red-700 transition-all"
+                >
+                  Despre terapie
+                </Link>
+              </div>
+              <div className="w-full flex flex-col gap-2 items-center">
+                <h2 className="font-semibold p-2 mb-2 w-full border-b border-black border-solid">
+                  Contacti-ma
+                </h2>
+                <Link
+                  href={"/contact"}
+                  className="py-2 px-6 w-full text-center bg-red-700 text-white rounded-lg"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
+      )}
     </>
   );
 };
