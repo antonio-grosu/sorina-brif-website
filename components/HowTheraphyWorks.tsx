a5dcc27c336dad4c9656903bc42315f2b9d5060e
Tirdea Dominic <dominic.tirdea2@gmail.com>
2025-03-20 23:41:27 +0200

    inceput how-theraphy-works-component

---
+++ components/HowTheraphyWorks.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { use, useState } from "react";
+import { useEffect } from "react";
+import Link from "next/link";
+
+export const HowTheraphyWorks = () => {
+  return (
+    <>
+      <div className="w-4/5 h-20 ml-16 bg-rose-100">
+        <h1 className="text-3xl font-bold">Cum functioneaza terapia?</h1>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
