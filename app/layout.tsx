7020a15c56ecdf263a3c33b3b09e39077e604212
Tirdea Dominic <dominic.tirdea2@gmail.com>
2025-03-21 16:45:51 +0200

    contact form componnent done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
+import { ContactFormComponent } from "@/components/ContactFormComponent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <h1>Hello</h1>
+        <ContactFormComponent />
         {children}
       </body>
     </html>
---
+++ components/ContactFormComponent.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React from "react";
+
+export const ContactFormComponent = () => {
+  return (
+    <>
+      <div className=" bg-rose-100 text-black">
+        <form action="" className="w-full">
+          {/*  */}
+          <div className="flex flex-col items-center md:items-start gap-10">
+            <div className="flex flex-col md:flex-row md:justify-between gap-10 md:gap-20 h-full w-full">
+              <div className="flex flex-col gap-10">
+                <div className="flex flex-col  gap-4">
+                  <label htmlFor="name" className="font-semibold">
+                    Nume
+                  </label>
+                  <input
+                    className="bg-white pl-10 pr-12 py-2 rounded-md"
+                    type="text"
+                    id="name"
+                    placeholder="ex.Ion Popescu"
+                  />
+                </div>
+                <div className="flex flex-col gap-4 ">
+                  <label htmlFor="mail" className="font-semibold">
+                    Email
+                  </label>
+                  <input
+                    type="mail"
+                    className="bg-white py-2 pl-10 rounded-md"
+                    id="mail"
+                    placeholder="ex.ion.popescu@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="mesaj" className="font-semibold">
+                  Mesaj
+                </label>
+                <textarea
+                  name="mesaj"
+                  id="mesaj"
+                  className="bg-white h-full rounded-md px-10 resize-none"
+                ></textarea>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="bg-red-700 text-white font-semibold rounded-lg px-16 py-2 text-center"
+              >
+                Sumbit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
