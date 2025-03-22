502951fc39393528f45e48444ed9260af97584cd
Tirdea Dominic <dominic.tirdea2@gmail.com>
2025-03-22 17:00:12 +0200

    contact form section done unresponsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { ContactFormComponent } from "@/components/ContactFormComponent";
+import { ContactFormSection } from "@/components/ContactFormSection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <ContactFormComponent />
+        <ContactFormSection />
         {children}
       </body>
     </html>
